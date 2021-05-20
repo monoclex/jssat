50800f72a5285f72e96deb6bdab848f35454ac7d
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2021-05-20 19:36:36 -0400

    Setup structure, parse script

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    input::StringInput,
+    sync::Lrc,
+    FileName, SourceMap,
+};
+use swc_ecmascript::parser::{Parser, Syntax};
+
+mod ast_traversal;
+mod ir;
+
+fn main() {
+    let file_name = std::env::args()
+        .into_iter()
+        .skip(1)
+        .next()
+        .expect("expected file name");
+
+    let content = std::fs::read_to_string(file_name).expect("expected to read file");
+
+    // https://github.com/Starlight-JS/starlight/blob/4c4ce5d0178fb28c3b2a044d572473baaf057b73/crates/starlight/src/vm.rs#L275-L300
+    let cm: Lrc<SourceMap> = Default::default();
+    let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
+    let fm = cm.new_source_file(FileName::Anon, content);
+
+    let mut parser = Parser::new(
+        Syntax::Es(Default::default()),
+        StringInput::from(fm.as_ref()),
+        None,
+    );
+
+    for err in parser.take_errors() {
+        err.into_diagnostic(&handler).emit();
+    }
+
+    let script = parser.parse_script().expect("script to parse");
+
+    println!("{:#?}", script);
+}
