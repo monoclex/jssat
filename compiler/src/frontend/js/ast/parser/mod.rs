0a428d2393e9da33529402b7c8156396eb255e5e
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2021-11-20 20:04:46 -0500

    cleanup deps

--- compiler/src/frontend/js/ast/parser/mod.rs
+++ compiler/src/frontend/js/ast/parser/mod.rs
@@ -1,37 +1,29 @@
 mod exprs;
 mod stmts;
 
-use swc_common::{
-    errors::{ColorConfig, Handler},
-    input::StringInput,
-    sync::Lrc,
-    FileName, SourceMap,
-};
+use std::convert::TryInto;
+
+use swc_common::{input::StringInput, BytePos};
 use swc_ecmascript::parser::{PResult, Parser, Syntax};
 
 use super::parse_nodes as js;
 use swc_ecmascript::ast as swc;
 
 pub fn parse_script(script: &str) -> PResult<js::Script> {
-    let swc_script = to_swc_script(script.to_string())?;
+    let swc_script = to_swc_script(script)?;
     Ok(swc_script.to_parse_node())
 }
 
-fn to_swc_script(source: String) -> PResult<swc::Script> {
-    // https://github.com/Starlight-JS/starlight/blob/4c4ce5d0178fb28c3b2a044d572473baaf057b73/crates/starlight/src/vm.rs#L275-L300
-    let cm: Lrc<SourceMap> = Default::default();
-    let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
-    let fm = cm.new_source_file(FileName::Anon, source);
-
+fn to_swc_script(source: &str) -> PResult<swc::Script> {
     let mut parser = Parser::new(
-        Syntax::Es(Default::default()),
-        StringInput::from(fm.as_ref()),
+        Default::default(),
+        StringInput::new(
+            source,
+            BytePos(0),
+            BytePos(source.len().try_into().unwrap()),
+        ),
         None,
     );
-
-    for err in parser.take_errors() {
-        err.into_diagnostic(&handler).emit();
-    }
 
     parser.parse_script()
 }
